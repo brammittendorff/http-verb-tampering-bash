e6d78a5902ddd9f47fe1e3cdc6bfd05d15b9f1fb
Bram Mittendorff <b.mittendorff@nerds.company>
2016-01-19 21:45:48 +0100

    removed gzip

--- tampering.sh
+++ tampering.sh
@@ -8,7 +8,7 @@ PORT=$3
 
 if [ ! -z $HOST ] && [ ! -z $PATH ] && [ ! -x $PORT ]; then
         for TYPE in ${REQUEST_TYPES[@]}; do
-                echo -e "$TYPE $PATH HTTP/1.1\nHost: $HOST\nConnection: keep-alive\nCache-Control: max-age=0\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\nUpgrade-Insecure-Requests: 1\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36\nAccept-Encoding: gzip, deflate, sdch\nAccept-Language: en-US,en;q=0.8,nl;q=0.6\n" > "$TYPE.http.txt"
+                echo -e "$TYPE $PATH HTTP/1.1\nHost: $HOST\nConnection: keep-alive\nCache-Control: max-age=0\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\nUpgrade-Insecure-Requests: 1\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36\nAccept-Language: en-US,en;q=0.8,nl;q=0.6\n" > "$TYPE.http.txt"
                 echo "nc $HOST $PORT < $TYPE.http.txt"
         done
 else
